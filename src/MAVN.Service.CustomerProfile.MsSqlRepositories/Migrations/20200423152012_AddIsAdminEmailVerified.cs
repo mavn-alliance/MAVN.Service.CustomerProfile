e7f4bda7e8ac03baf18e2afca9bbbb3bca0b1b06
Vitalii Dasaev <vitalii.dasaev@lykke-business.ch>
2020-04-23 18:23:55 +0300

    #10 add migration for admin IsEmailVerified

---
+++ src/MAVN.Service.CustomerProfile.MsSqlRepositories/Migrations/20200423152012_AddIsAdminEmailVerified.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MAVN.Service.CustomerProfile.MsSqlRepositories.Migrations
+{
+    public partial class AddIsAdminEmailVerified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "email_verified",
+                schema: "customer_profile",
+                table: "admin_profiles_archive",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "was_email_ever_verified",
+                schema: "customer_profile",
+                table: "admin_profiles_archive",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "email_verified",
+                schema: "customer_profile",
+                table: "admin_profiles",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "was_email_ever_verified",
+                schema: "customer_profile",
+                table: "admin_profiles",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email_verified",
+                schema: "customer_profile",
+                table: "admin_profiles_archive");
+
+            migrationBuilder.DropColumn(
+                name: "was_email_ever_verified",
+                schema: "customer_profile",
+                table: "admin_profiles_archive");
+
+            migrationBuilder.DropColumn(
+                name: "email_verified",
+                schema: "customer_profile",
+                table: "admin_profiles");
+
+            migrationBuilder.DropColumn(
+                name: "was_email_ever_verified",
+                schema: "customer_profile",
+                table: "admin_profiles");
+        }
+    }
+}
