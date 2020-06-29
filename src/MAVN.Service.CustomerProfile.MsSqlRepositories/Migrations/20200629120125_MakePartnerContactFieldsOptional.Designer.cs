2cfe4f415fee7ce71606d705510af64e51b9036c
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-06-29 15:02:05 +0300

    make partner cotnact email optional

--- src/MAVN.Service.CustomerProfile.MsSqlRepositories/Migrations/20200629120125_MakePartnerContactFieldsOptional.Designer.cs
+++ src/MAVN.Service.CustomerProfile.MsSqlRepositories/Migrations/20200629120125_MakePartnerContactFieldsOptional.Designer.cs
@@ -4,14 +4,16 @@ using MAVN.Service.CustomerProfile.MsSqlRepositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MAVN.Service.CustomerProfile.MsSqlRepositories.Migrations
 {
     [DbContext(typeof(CustomerProfileContext))]
-    partial class CustomerProfileContextModelSnapshot : ModelSnapshot
+    [Migration("20200629120125_MakePartnerContactFieldsOptional")]
+    partial class MakePartnerContactFieldsOptional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
